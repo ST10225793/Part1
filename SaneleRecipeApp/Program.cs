6a50ae5a8fe616414d4db7e00edf97bdf7da7e3c
lab_services_student <lab_services_student@lab000000>
2023-04-26 08:39:22 +0000

    new menu created using if-while loops

--- SaneleRecipeApp/Program.cs
+++ SaneleRecipeApp/Program.cs
@@ -24,42 +24,45 @@ namespace SaneleRecipeApp
 
             Console.WriteLine();
 
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("\t" + "Please select one of the following menu items: ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("(1) Enter details of recipe");
-            Console.WriteLine("(2) Display full recipe");
-            Console.WriteLine("(3) Change recipe scaling");
-            Console.WriteLine("(4) Reset quatities to original values");
-            Console.WriteLine("(5) Clear data to enter new recipe");
-            Console.WriteLine("");
-            Console.WriteLine("Make a choice");
-            menu = Convert.ToInt32(Console.ReadLine());
-
-            switch (menu)
+            do
             {
-                case 1:
-                    Console.WriteLine();
-                    obj.DisplayMenu(menu);
-                    break;
-                case 2:
-                    Console.WriteLine();
-                    obj.DisplayMenu(menu);
-                    break;
-                case 3:
-                    Console.WriteLine();
-                    obj.DisplayMenu(menu);
-                    break;
-                case 4:
-                    Console.WriteLine();
-                    obj.DisplayMenu(menu);
-                    break;
-                case 5:
-                    Console.WriteLine();
-                    obj.DisplayMenu(menu);
-                    break;
-            }
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("\t" + "Please select one of the following menu items: ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("(1) Enter details of recipe");
+                Console.WriteLine("(2) Display full recipe");
+                Console.WriteLine("(3) Change recipe scaling");
+                Console.WriteLine("(4) Reset quatities to original values");
+                Console.WriteLine("(5) Clear data to enter new recipe");
+                Console.WriteLine("");
+                Console.WriteLine("Make a choice");
+                menu = Convert.ToInt32(Console.ReadLine());
+                if (menu == 1)
+                {
+                    RecipeDetails.StoreRecipe(); 
+                }
+                if(menu == 2)
+                {
+
+                }
+                if(menu == 3)
+                {
+
+                }
+                if(menu == 4)
+                {
+
+                }
+                if(menu == 5)
+                {
+
+                }
+                else if (menu == 6)
+                {
+                    Environment.Exit(0);
+                }
+
+            } while(true);
         }
     }
 }
--- SaneleRecipeApp/RecipeDetails.cs
+++ SaneleRecipeApp/RecipeDetails.cs
@@ -20,17 +20,6 @@ namespace SaneleRecipeApp
         {
             //Display menu method created so that program continues to run after user is doe with an entry
             
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("\t" + "Please select one of the following menu items: ");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("(1) Enter details of recipe");
-            Console.WriteLine("(2) Display full recipe");
-            Console.WriteLine("(3) Change recipe scaling");
-            Console.WriteLine("(4) Reset quatities to original values");
-            Console.WriteLine("(5) Clear data to enter new recipe");
-            Console.WriteLine("");
-            Console.WriteLine("Make a choice");
-            menu = Convert.ToInt32(Console.ReadLine());
             return "";
         }
         public static void StoreRecipe()
