a46b74e4c4875ed3dc0cf40962f597c2d4ab58d1
lab_services_student <lab_services_student@lab000000>
2023-04-26 05:47:53 +0000

    Switch case created

--- SaneleRecipeApp/Program.cs
+++ SaneleRecipeApp/Program.cs
@@ -29,6 +29,19 @@ namespace SaneleRecipeApp
             Console.WriteLine("Make a choice");
             menu = Convert.ToInt32(Console.ReadLine());
 
+            switch (menu)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+            }
         }
     }
 }
