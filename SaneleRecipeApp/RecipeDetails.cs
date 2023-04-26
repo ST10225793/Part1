3147c5b7040e7206ec0fb26ec543d0faecd289e2
lab_services_student <lab_services_student@lab000000>
2023-04-26 06:07:18 +0000

    Store details method created

--- SaneleRecipeApp/RecipeDetails.cs
+++ SaneleRecipeApp/RecipeDetails.cs
@@ -33,5 +33,42 @@ namespace SaneleRecipeApp
             menu = Convert.ToInt32(Console.ReadLine());
             return "";
         }
+        public static void StoreRecipe()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Enter name of recipe");
+            RecipeName = Console.ReadLine();
+            Console.WriteLine("Enter number of ingredients");
+            NumofIngredients = Convert.ToInt32(Console.ReadLine());
+            IngrName = new string[NumofIngredients];
+            IngrQuantity = new int[NumofIngredients];
+            UnitofMeasurement = new string[NumofIngredients];
+
+            for (int i = 0; i < NumofIngredients; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ingredient details");
+                Console.WriteLine($"Ingredient no.{i + 1}");
+
+                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.WriteLine("Enter name of ingredient");
+                IngrName[i] = Console.ReadLine();
+                Console.WriteLine("Enter quantity of ingredient");
+                IngrQuantity[i] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter unit of measurement");
+                UnitofMeasurement[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Enter number of steps needed for this recipe");
+            NumofSteps = Convert.ToInt32(Console.ReadLine());
+            StepDescription = new String[NumofSteps];
+            for (int j = 0; j < NumofSteps; j++)
+            {
+                Console.WriteLine($"Enter the description for each step {j + 1}");
+                StepDescription[j] = Console.ReadLine();
+
+            }
+        }
     }
 }
