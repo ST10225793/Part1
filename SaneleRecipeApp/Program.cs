a91ca3785d2c2ff42b59c40fe0a83c7a0ff9c0d2
lab_services_student <lab_services_student@lab000000>
2023-04-26 05:44:47 +0000

    Add project files.

---
+++ SaneleRecipeApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaneleRecipeApp
+{
+    internal class Program
+    {
+        static int menu;
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Welcome to Sanele's Recipe App <3");
+
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Please select one of the following menu items: ");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("(1) Enter details of recipe");
+            Console.WriteLine("(2) Display full recipe");
+            Console.WriteLine("(3) Change recipe scaling");
+            Console.WriteLine("(4) Reset quatities to original values");
+            Console.WriteLine("(5) Clear data to enter new recipe");
+            Console.WriteLine("");
+            Console.WriteLine("Make a choice");
+            menu = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
