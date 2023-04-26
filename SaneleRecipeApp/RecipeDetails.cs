68e8980453359f019683f6f9b774fcf9702b6378
lab_services_student <lab_services_student@lab000000>
2023-04-26 05:51:19 +0000

    Display menu method created

--- SaneleRecipeApp/RecipeDetails.cs
+++ SaneleRecipeApp/RecipeDetails.cs
@@ -8,5 +8,12 @@ namespace SaneleRecipeApp
 {
     internal class RecipeDetails
     {
+        private static String RecipeName;
+        private static int NumofIngredients;
+        private static String[] IngrName;
+        private static int[] IngrQuantity;
+        private static String[] UnitofMeasurement;
+        private static int NumofSteps;
+        private static String[] StepDescription;
     }
 }
